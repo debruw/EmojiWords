e3febe070ab6c53649737ff31dc7fd7c9f835957
Ebru Çakar <ebru_cakar_11@hotmail.com>
2020-11-02 19:11:36 +0300

    GameType 2 yapıldı. Cümle tamamlama.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using TapticPlugin;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -21,25 +23,15 @@ public class GameManager : MonoBehaviour
 
     public void RetryClick()
     {
-
-    }
-
-    public void RightImageClicked()
-    {        
-        Confetti.SetActive(true);
-        StartCoroutine(WaitAndOpenWinPanel());
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     IEnumerator WaitAndOpenWinPanel()
     {
+        Confetti.SetActive(true);
         yield return new WaitForSeconds(1);
         LeanCanvas.SetActive(false);
         WinPanel.SetActive(true);
-    }
-
-    public void WrongImageClicked()
-    {
-        StartCoroutine(WaitAndOpenLosePanel());
     }
 
     IEnumerator WaitAndOpenLosePanel()
@@ -48,6 +40,81 @@ public class GameManager : MonoBehaviour
         LosePanel.SetActive(true);
         LeanCanvas.SetActive(false);
     }
+
+    #region Game type 1
+    public void RightImageClicked()
+    {
+        StartCoroutine(WaitAndOpenWinPanel());
+    }
+
+    public void WrongImageClicked()
+    {
+        StartCoroutine(WaitAndOpenLosePanel());
+    }
+
+    #endregion
+
+    #region Game type 2
+    Transform firstRectTransform;
+    GameObject selectedDragBox;
+    bool isOnHower = false;
+    bool isHoldingDragBox = false;
+    public GameObject WrongPanel;
+
+    public void DragboxOnBegin(GameObject sdb)
+    {
+        firstRectTransform = sdb.transform;
+        selectedDragBox = sdb;
+        isHoldingDragBox = true;
+    }
+
+    public Text HoverText;
+    public void DragboxOnEnd(bool bl)
+    {
+        isHoldingDragBox = false;
+        SendRaycast();
+
+        if (isOnHower)
+        {//Right place
+            if (bl)
+            {//Right word                
+                HoverText.text = selectedDragBox.GetComponentInChildren<Text>().text;
+                selectedDragBox.SetActive(false);
+                StartCoroutine(WaitAndOpenWinPanel());
+            }
+            else
+            {//Wrong word
+                WrongPanel.GetComponent<Animation>().Play();
+                StartCoroutine(WaitAndOpenLosePanel());
+            }
+        }
+        else
+        {//Wrong place
+            selectedDragBox.transform.position = firstRectTransform.transform.position;
+        }
+    }
+
+    public GraphicRaycaster gr;
+    public void SendRaycast()
+    {
+        //Create the PointerEventData with null for the EventSystem
+        PointerEventData ped = new PointerEventData(null);
+        //Set required parameters, in this case, mouse position
+        ped.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        //Create list to receive all results
+        List<RaycastResult> results = new List<RaycastResult>();
+        //Raycast it
+        gr.Raycast(ped, results);
+        foreach (RaycastResult rr in results)
+        {
+            if (rr.gameObject.CompareTag("Hover"))
+            {
+                isOnHower = true;
+            }
+        }
+    }
+
+    #endregion
 
     public GameObject VibrationButtonImage;
     public Sprite on, off;
